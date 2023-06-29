9af17311236988c7bc951e20ffa0604b854869f7
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2023-06-29 16:12:26 +0300

    Add CustomerAppealType to customers table and Nova
    This commit introduces a new feature for customer profiles. A new enum 'appeal_type' was included in the database migration for the customers table. Moreover, the CustomerAppealType has been added to the Nova options for both 'renter' and 'passenger' customer types in the Customer Nova resource. This allows admin users to track and filter customers based on their preferred form of address.

---
+++ app/Enums/CustomerAppealType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+class CustomerAppealType extends Enum
+{
+    public const PONES = 'mis';
+    public const PONNIAH = 'mr';
+
+    public static function values(): array
+    {
+        return [
+            self::PONES,
+            self::PONNIAH,
+        ];
+    }
+
+    public static function labels(): array
+    {
+        return [
+            self::PONES => __('Ponas'),
+            self::PONNIAH => __('Ponia'),
+        ];
+    }
+
+    public static function isValid($value): bool
+    {
+        return in_array($value, self::values());
+    }
+}
