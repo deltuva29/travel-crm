791a268fc2150c35ea771f471e9854d67d6aaaa8
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2023-07-03 23:05:58 +0300

    Add updated in trip migration

---
+++ database/migrations/2023_07_03_230145_add_new_column_completed_at_in_trips_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trips', function (Blueprint $table) {
+            $table->timestamp('completed_at')->nullable()->after('payment_method_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trips', function (Blueprint $table) {
+            $table->dropColumn('payment_method_id');
+        });
+    }
+};
