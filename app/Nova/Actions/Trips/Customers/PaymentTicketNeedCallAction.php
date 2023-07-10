23aee4172a7f5983eeeb099665974ba451180577
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2023-07-11 01:37:54 +0300

    Add call action to TripCustomer in Nova
    In TripCustomer Nova Resource, a new action called 'PaymentTicketNeedCallAction' has been added to give the administrators an option to mark a customer for a call. A check is also implemented to show this action only when it's necessary. The commit addresses the need for a manual follow-up system in case of any issues with payment.

---
+++ app/Nova/Actions/Trips/Customers/PaymentTicketNeedCallAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Nova\Actions\Trips\Customers;
+
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+
+class PaymentTicketNeedCallAction extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    public function name(): string
+    {
+        return __('Skambutis dalyviui');
+    }
+
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        return $this->processModels($fields, $models);
+    }
+
+    private function processModels(ActionFields $fields, Collection $models): array
+    {
+        try {
+            foreach ($models as $model) {
+                $this->paidModel($fields, $model);
+                if ($model->isNeedCall()) {
+                    return Action::message(__('Susisieksime su ' . $model->getCustomerFullName() . '.'));
+                } else {
+                    return Action::message(__('Skambutis ' . $model->getCustomerFullName() . ' atšauktas.'));
+                }
+            }
+        } catch (Exception $e) {
+            return Action::danger(__('Klaida nustatant skambutį.'));
+        }
+        return [];
+    }
+
+    private function paidModel(ActionFields $fields, $model): void
+    {
+        $model->update([
+            'need_call' => !$model->need_call
+        ]);
+    }
+
+    public function fields(): array
+    {
+        return [];
+    }
+}
