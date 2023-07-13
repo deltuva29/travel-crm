56d2e3e988252b143eaeaa2dcd71ca9f583f8377
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2023-07-13 23:32:35 +0300

    Implement sorting feature to the Trip model

--- app/Nova/Trip.php
+++ app/Nova/Trip.php
@@ -16,12 +16,17 @@ use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Panel;
+use MichielKempen\NovaOrderField\Orderable;
 use NovaAttachMany\AttachMany;
 use OptimistDigital\NovaSimpleRepeatable\SimpleRepeatable;
 
 class Trip extends Resource
 {
+    use Orderable;
+
     public static string $model = \App\Models\Trip::class;
+
+    public static string $defaultOrderField = 'order';
 
     public function title(): string
     {
@@ -163,6 +168,8 @@ class Trip extends Resource
                 ->resolveUsing(fn($value, $resource) => is_null($value) ? '0.00' : number_format($value, 2, '.', ''))
                 ->displayUsing(fn($value, $resource) => $resource->formatPrice())
                 ->asHtml(),
+
+//            OrderField::make(__('Rikiavimas'), 'id'),
 
             Text::make(__('Kelionės būsena'), fn() => $this->isAlreadyCompleted() ?
                 '<span class="flex items-center text-md"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="check-circle" role="presentation" class="fill-current text-success mr-1"><path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"></path></svg> ' . $this->getCompletedAtDateTime() . '</span>'
