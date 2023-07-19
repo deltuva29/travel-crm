8f37a54d74d13ff32517bdea2dbbf517d478674f
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2023-07-19 21:21:52 +0300

    Update header links and add new routes

---
+++ app/Http/Livewire/Customers/Profile/CustomerProfileSettingsForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire\Customers\Profile;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+class CustomerProfileSettingsForm extends Component
+{
+    public function render(): Factory|View|Application
+    {
+        return view('livewire.customers.profile.customer-profile-settings-form');
+    }
+}
