c77a2e579ece1d58d1ac253f5a41fb9bce2a8ab4
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2023-07-20 11:59:25 +0300

    Improve page loading transition and update interface style

--- app/Http/Livewire/Customers/Profile/CustomerProfileSettingsForm.php
+++ app/Http/Livewire/Customers/Profile/CustomerProfileSettingsForm.php
@@ -9,6 +9,19 @@ use Livewire\Component;
 
 class CustomerProfileSettingsForm extends Component
 {
+    public $isLoading = false;
+
+    protected $listeners = [
+        'loaded' => 'doLoading'
+    ];
+
+    public function doLoading(): void
+    {
+        $this->isLoading = true;
+        $this->dispatchBrowserEvent('start-loading');
+        $this->emit('loaded');
+    }
+
     public function render(): Factory|View|Application
     {
         return view('livewire.customers.profile.customer-profile-settings-form');
