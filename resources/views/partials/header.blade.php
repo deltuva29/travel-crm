04cd937dc5f9e230cf94b234a1a69e3c7e66783f
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2023-07-20 07:02:58 +0300

    Update text color in header dropdown menus

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -46,7 +46,7 @@
             >
                 <li>
                     <a
-                        class="flex items-center px-4 py-1 mx-2 text-sm tracking-wide bg-amber-200 hover:bg-amber-300 text-gray-700 rounded-md transition-all"
+                        class="flex items-center px-4 py-1 mx-2 text-sm tracking-wide bg-amber-200 hover:bg-amber-300 text-[#24455C] rounded-md transition-all"
                         href="{{ route('customer.profile') }}"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-2">
@@ -75,14 +75,14 @@
                     x-transition:leave-end="opacity-0 transform scale-95"
                     class="absolute right-0 mt-2 pt-2 w-44 bg-white rounded-md text-center shadow-xl z-20" style="box-shadow: 0 3px 6px 0 rgba(0,0,0,.2);"
                 >
-                    <a href="{{ route('customer.profile.settings') }}" class="flex items-center px-3 py-2 mx-2 text-xs rounded-md text-gray-700 bg-gray-300 hover:bg-gray-200 transition-all">
+                    <a href="{{ route('customer.profile.settings') }}" class="flex items-center px-3 py-2 mx-2 text-xs rounded-md text-[#24455C] bg-gray-300 hover:bg-gray-200 transition-all">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3 mr-1">
                             <path fill-rule="evenodd" d="M8.34 1.804A1 1 0 019.32 1h1.36a1 1 0 01.98.804l.295 1.473c.497.144.971.342 1.416.587l1.25-.834a1 1 0 011.262.125l.962.962a1 1 0 01.125 1.262l-.834 1.25c.245.445.443.919.587 1.416l1.473.294a1 1 0 01.804.98v1.361a1 1 0 01-.804.98l-1.473.295a6.95 6.95 0 01-.587 1.416l.834 1.25a1 1 0 01-.125 1.262l-.962.962a1 1 0 01-1.262.125l-1.25-.834a6.953 6.953 0 01-1.416.587l-.294 1.473a1 1 0 01-.98.804H9.32a1 1 0 01-.98-.804l-.295-1.473a6.957 6.957 0 01-1.416-.587l-1.25.834a1 1 0 01-1.262-.125l-.962-.962a1 1 0 01-.125-1.262l.834-1.25a6.957 6.957 0 01-.587-1.416l-1.473-.294A1 1 0 011 10.68V9.32a1 1 0 01.804-.98l1.473-.295c.144-.497.342-.971.587-1.416l-.834-1.25a1 1 0 01.125-1.262l.962-.962A1 1 0 015.38 3.03l1.25.834a6.957 6.957 0 011.416-.587l.294-1.473zM13 10a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd"/>
                         </svg> {{ __('Paskyros nustatymai') }}
                     </a>
                     @auth('customer')
                         <a href="{{ route('logout') }}"
-                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="flex items-center justify-center px-4 py-2 text-sm text-gray-700 hover:text-gray-500 transition-all">
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="flex items-center justify-center px-4 py-2 text-sm text-[#24455C] hover:text-gray-500 transition-all">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-1">
                                 <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clip-rule="evenodd"/>
                                 <path fill-rule="evenodd" d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z" clip-rule="evenodd"/>
