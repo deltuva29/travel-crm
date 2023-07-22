77ee41430eae04ea09a5de9166a4dd77fe376591
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2023-07-22 15:36:29 +0300

    Update customer profile UI elements

--- resources/views/livewire/customers/profile/customer-profile-page.blade.php
+++ resources/views/livewire/customers/profile/customer-profile-page.blade.php
@@ -51,7 +51,7 @@
                                 <div class="flex justify-center items-center mb-6">
                                     <div class="relative w-[125px] h-[140px] bg-[#d9dee8] bg-no-repeat bg-cover rounded-xl overflow-hidden bg-center" style="background-image: url('https://lh3.googleusercontent.com/a/AAcHTtd7GIlH4iKlD9SJy-z3lWmGpsVwKcjLn-Z5MPcviNIFkgM=s96-c');">
                                         <div class="absolute bottom-0 left-0 right-0">
-                                            <button class="relative w-full py-[7px] px-[25px] bg-[#e1e8ee] hover:bg-opacity-75 transition-all">{{ __('Įkelti') }}</button>
+                                            <button class="relative w-full py-[7px] px-[25px] text-white bg-amber-400 hover:bg-opacity-75 transition-all">{{ __('Įkelti') }}</button>
                                             <a class="w-13 h-13 bg-center bg-no-repeat absolute top-1/2 transform -translate-y-1/2 right-0.5 transition duration-200 p-2.5" href="#">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6 text-white">
                                                     <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"/>
@@ -60,8 +60,19 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="pl-0 lg:pl-4 pr-0 lg:pr-0">
-                                    <div class="text-2xl md:text-2xl tracking-wide font-normal truncate">{{ $user->getIndividualFullName() }}</div>
+                                <div class="w-full pl-0 lg:pl-4 pr-0 lg:pr-0">
+                                    <div class="flex items-center justify-between">
+                                        <div class="text-2xl md:text-2xl tracking-wide font-normal truncate">
+                                            {{ $user->getIndividualFullName() }}
+                                        </div>
+                                        <a class="flex items-center bg-amber-400 text-white hover:bg-amber-400/75 rounded-md py-2 px-3 transition-all" href="#">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-0 lg:mr-1">
+                                                <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z"/>
+                                                <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z"/>
+                                            </svg>
+                                            <span class="lg:block hidden">{{ __('Redaguoti') }}</span>
+                                        </a>
+                                    </div>
                                     <div class="flex items-center text-lg md:text-lg tracking-wide font-normal truncate">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-1">
                                             <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z"/>
