598e35eb54dc8f938d434fd2e605335f3f0e168a
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2024-01-08 13:34:42 +0200

    wip

--- resources/views/livewire/auth/home/login.blade.php
+++ resources/views/livewire/auth/home/login.blade.php
@@ -9,7 +9,7 @@
                     <div class="text-sm font-bold text-gray-700 tracking-wide">{{ __('El.paštas') }}</div>
                     <label>
                         <div class="relative z-0 w-full mb-2 group">
-                            <input wire:model.lazy="email" id="email" name="email" class="block py-2.5 px-0 w-full text-sm text-gray-600 bg-transparent border-0 border-b-2 dark:border-gray-300 dark:focus:border-yellow-400 focus:outline-none focus:ring-0 focus:border-yellow-400 peer @error('email') border-red-300 text-red-900 placeholder-red-300 focus:ring-red @enderror" placeholder=" "/>
+                            <input wire:model.lazy="email" id="email" name="email" class="block py-2.5 px-0 w-full text-sm text-gray-600 bg-transparent border-0 border-b-2 dark:border-gray-300 dark:focus:border-yellow-400 focus:outline-none focus:ring-0 focus:border-yellow-400 peer @error('email') border-red-300 placeholder-red-300 focus:ring-red @enderror" placeholder=" "/>
                         </div>
                     </label>
                     @if ($email)
@@ -32,9 +32,9 @@
                     </div>
                     <label>
                         <div x-data="{ show: true }" class="relative z-0 w-full mb-2 group">
-                            <input wire:model.lazy="password" id="password" name="password" :type="show ? 'password' : 'text'" class="block py-2.5 px-0 w-full text-sm text-gray-600 bg-transparent border-0 border-b-2 dark:border-gray-300 dark:focus:border-yellow-400 focus:outline-none focus:ring-0 focus:border-yellow-400 peer @error('password') border-red-300 text-red-900 placeholder-red-300 focus:ring-red @enderror" placeholder=" "/>
+                            <input wire:model.lazy="password" id="password" name="password" :type="show ? 'password' : 'text'" class="block py-2.5 px-0 w-full text-sm text-gray-600 bg-transparent border-0 border-b-2 dark:border-gray-300 dark:focus:border-yellow-400 focus:outline-none focus:ring-0 focus:border-yellow-400 peer @error('password') border-red-300 placeholder-red-300 focus:ring-red @enderror" placeholder=" "/>
                             <div class="absolute top-1/2 right-4 cursor-pointer transform -translate-y-1/2">
-                                <svg class="h-4 text-gray-400 hidden" fill="none" @click="show = !show" :class="{'hidden': !show, 'block':show }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                                <svg class="h-4 text-gray-300 hidden" fill="none" @click="show = !show" :class="{'hidden': !show, 'block':show }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                                     <path fill="currentColor" d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z">
                                     </path>
                                 </svg>
